18bb560233979a8badddc7d106fead7963619aee
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-08-23 21:45:58 -0500

    agregando cambios

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: 'https://ChiangArt.github.io/To-Do-New',
-})
+  base: "/To-Do-New/",
+});
